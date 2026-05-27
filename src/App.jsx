72ed3bd6acbd48b130c4bd9404c1038c21636238
sunny <sunnygolamshahariar@gmail.com>
2026-05-27 16:05:53 +0600

    New update

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Hero from './components/hero'
 import HomeCards from './components/HomeCards'
 import Card from './components/Card'
 import JobListings from './components/JobListings'
+import ViewAllJobs from './components/ViewAllJobs'
 const App = () => {
   return (
     <>
@@ -11,14 +12,7 @@ const App = () => {
     <Hero title='Find your job' subtitle="Unless you are a coder." />    
     <HomeCards/>
     <JobListings/>
-
-    <section className="m-auto max-w-lg my-10 px-6">
-      <a
-        href="jobs.html"
-        className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-        >View All Jobs</a
-      >
-    </section>
+    <ViewAllJobs/>
     </>
   )
 }
